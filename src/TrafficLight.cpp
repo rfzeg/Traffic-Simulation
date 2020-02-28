cb273d09197d497121342814e59f16e5455f6e7b
Roberto Zegers <rzegers@uc.cl>
2020-02-28 11:17:51 +0100

    feat: implement cycleThroughPhases() to switch the phase of the traffic light

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <chrono>  // to measure elapsed timecd build
 #include "TrafficLight.h"
 
 /* Implementation of class "MessageQueue" */
@@ -46,10 +47,35 @@ void TrafficLight::simulate()
 }
 
 // virtual function which is executed in a thread
-void TrafficLight::cycleThroughPhases()
-{
-    // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
-    // and toggles the current phase of the traffic light between red and green and sends an update method 
-    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+void TrafficLight::cycleThroughPhases(){
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(4,6); // set range 4 to 6 seconds
+
+    // Record start time, initialize finish time
+    auto start = std::chrono::high_resolution_clock::now();
+    auto finish = std::chrono::high_resolution_clock::now();
+    int cycle_duration = 5; // set first cycle to be 5 sec
+
+    while(true){
+        if (std::chrono::duration_cast<std::chrono::seconds>(finish - start).count() > cycle_duration){
+
+            // flip light: if red make it green, if green make it red
+            int new_phase = abs(TrafficLight::getCurrentPhase() - 1);
+            // static_cast operator explicitly converts an int value to an enum type
+            _currentPhase = static_cast<TrafficLightPhase>(new_phase);
+
+            // TO-DO: Send update message to the class `MessageQueue`
+            // to be implemented as part of assignment FP.3
+
+            // generate next cycle duration (range was set 4 to 6 seconds)
+            int cycle_duration = distribution(generator)*1000;
+
+            // update start time
+            start = std::chrono::high_resolution_clock::now();
+        }
+
+       std::this_thread::sleep_for(std::chrono::milliseconds(1)); // 100 milliseconds = 0.1 second
+       // update finish time
+       finish = std::chrono::high_resolution_clock::now();
+    }
 }
